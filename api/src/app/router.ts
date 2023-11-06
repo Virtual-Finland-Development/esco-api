20a4e1f6790cf2397a0a460e7a661416405af90b
Lassi Piironen <lsipii@kapsi.fi>
2023-11-06 16:00:05 +0200

    refactor: router does not exec

--- api/src/app/router.ts
+++ api/src/app/router.ts
@@ -3,22 +3,22 @@ import getCuratedOccupations from "./actions/get-curated-occupations";
 import getSkills from "./actions/get-skills";
 import healthCheck from "./actions/health-check";
 
-export default function execute(request: Request): Promise<Response> {
+export default function getAction(request: Request): (request: Request) => Promise<Response> {
   const method = request.method;
   const path = new URL(request.url).pathname;
 
   switch (`${method} ${path}`) {
     case "GET /":
     case "GET /health-check":
-      return healthCheck(request);
+      return healthCheck;
     case "GET /skills":
-      return getSkills(request);
+      return getSkills;
     case "GET /occupations":
-      return getAllOccupations(request);
+      return getAllOccupations;
     case "POST /Employment/EscoOccupations_v1.0":
     case "POST /Employment/EscoOccupations_v0.1":
-      return getCuratedOccupations(request);
+      return getCuratedOccupations;
     default:
-      return Promise.resolve(new Response("Not found", { status: 404 }));
+      throw new Error("Not found"); // @TODO
   }
 }
--- api/src/index.ts
+++ api/src/index.ts
@@ -1,9 +1,10 @@
-import execute from "./app/router";
+import getAction from "./app/router";
 
 export default {
   async fetch(request: Request): Promise<Response> {
     try {
-      return await execute(request);
+      const action = getAction(request);
+      return await action(request);
     } catch (error: any) {
       // @TODO: logging, metrics, etc.
       return new Response(error.message, { status: 500 });
